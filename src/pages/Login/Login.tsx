11b9fa452f069ca8197a3d0f01dd932bf587d709
Armando Rivera <rivera.armando997@gmail.com>
2023-05-02 19:00:43 -0500

    useRef and forms

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,38 +1,56 @@
+import { useRef } from "react";
 import styles from "./login.module.scss";
 import logo from "../../icons/logo_yard_sale.svg";
 
 export const Login = () => {
+  const form = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = (event: Event) => {
+    event.preventDefault();
+    const formData = new FormData(form.current as unknown as HTMLFormElement);
+    const data = {
+      usename: formData.get("email"),
+      password: formData.get("password"),
+    };
+    console.log(data);
+  };
+
   return (
-    <div className={styles.login}>
-      <div className={styles["form-container"]}>
-        <img src={logo} alt="logo" className={styles.logo} />
-        <h1 className={styles.title}>Create a new password</h1>
-        <p className={styles.subtitle}>Enter a new password for your account</p>
-        <form action="/" className={styles.form}>
+    <div className={styles.Login}>
+      <div className={styles["Login-container"]}>
+        <img src={logo} alt="logo" className="logo" />
+        <form action="/" className={styles.form} ref={form}>
+          <label htmlFor="email" className="label">
+            Email address
+          </label>
+          <input
+            type="text"
+            name="email"
+            placeholder="armando@example.com"
+            className={`${styles.input} ${styles["input-email"]}`}
+          />
           <label htmlFor="password" className={styles.label}>
             Password
           </label>
           <input
             type="password"
-            id="password"
+            name="password"
             placeholder="*********"
             className={`${styles.input} ${styles["input-password"]}`}
           />
-          <label htmlFor="new-password" className={styles.label}>
-            Password
-          </label>
-          <input
-            type="password"
-            id="new-password"
-            placeholder="*********"
-            className={`${styles.input} ${styles["input-password"]}`}
-          />
-          <input
-            type="submit"
-            defaultValue="Confirm"
-            className={`${styles["primary-button"]} ${styles["login-button"]}`}
-          />
+          <button
+            onClick={(event) => handleSubmit(event as unknown as Event)}
+            className={`${styles["primary-button"]} ${styles["login-button"]}"`}
+          >
+            Log in
+          </button>
+          <a href="/">Forgot my password</a>
         </form>
+        <button
+          className={`${styles["secondary-button"]} ${styles["signup-button"]}`}
+        >
+          Sign up
+        </button>
       </div>
     </div>
   );
